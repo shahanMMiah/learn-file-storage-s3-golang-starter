cbbc10fdeec3ea67e297ed1f590d0101b67ec379
shahanMMiah <shahan.m.miah@gmail.com>
2025-09-23 23:12:27 +0100

    thumbnail file server handle

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +33,48 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const storageSize = 10 << 20
+	r.ParseMultipartForm(storageSize)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "file not found from key thumbnail", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	vid, err := cfg.db.GetVideo(videoID)
+
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorised user", err)
+		return
+	}
+
+	contentType := strings.Split(header.Header.Get("Content-Type"), "/")
+	extension := contentType[len(contentType)-1]
+
+	thumbnailPath := filepath.Join(
+		cfg.assetsRoot,
+		fmt.Sprintf("%s.%s", videoID.String(), extension))
+
+	thmbFile, err := os.Create(thumbnailPath)
+
+	if err != nil {
+		respondWithError(w, http.StatusCreated, fmt.Sprintf("unable to create file %s", thumbnailPath), err)
+		return
+	}
+
+	_, err = io.Copy(thmbFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusCreated, fmt.Sprintf("unable to copy thumbnail to %s", thumbnailPath), err)
+		return
+	}
+
+	thmburl := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoID, extension)
+
+	vid.ThumbnailURL = &thmburl
+	err = cfg.db.UpdateVideo(vid)
+	payload, err := json.Marshal(vid)
+
+	respondWithJSON(w, http.StatusOK, payload)
 }
